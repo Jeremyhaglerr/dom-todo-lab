24206ccf4b75e36d3db3e37f4751b6fae97d605b
Jeremyhaglerr <jeremyhaglerr@gmail.com>
2022-02-03 01:52:45 -0600

    Initialize Repo

---
+++ js/app.js
@@ -0,0 +1,8 @@
+//cached element references
+const inputElement = document.getElementById("input1")
+const buttonElement = document.getElementById("submit-button")
+const resetElement = document.getElementById("reset")
+const listElement = document.getElementById("todo-list") 
+
+
+
